7e1b7525978e3ffd54d9cc53a149b6b147c14c4f
Brent O'Connor <epicserve@gmail.com>
2019-12-01 15:52:00 -0600

    Add first attempt at using parcel instead of gulp for building assets

--- src/js/base.js
+++ src/js/base.js
@@ -1,2 +1,11 @@
-$ = jQuery = require('jquery');
-var bootstrap = require('bootstrap/dist/js/bootstrap.bundle');
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery; // workaround for https://github.com/parcel-bundler/parcel/issues/333
+import 'bootstrap/dist/js/bootstrap.bundle';
+const myArr = [1, 2, 3].map((n) => n + 1);
+if (myArr.toString() !== '2,3,4') {
+  console.error("Array didn't transpile.");
+}
+if ($().modal === undefined) {
+  debugger;
+  console.error("Bootstrap wasn't imported correctly.");
+}

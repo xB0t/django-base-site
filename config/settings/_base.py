3d53647477ddbe8aa5ab19ddd07195ea5d6ed53a
Brent O'Connor <epicserve@gmail.com>
2023-01-04 15:28:41 -0600

    Move the template directory to the frontend directory

--- config/settings/_base.py
+++ config/settings/_base.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "frontend" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
